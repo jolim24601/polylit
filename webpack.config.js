9977ee5a9c4c2cc1540ceb5c95d292c9a98bcfb6
John Lim <jolim24601@gmail.com>
2016-01-25 00:01:31 -0500

    Add react hot loader module.

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
+        loader: ['react-hot', 'babel'],
         query: {
           presets: ['react']
         }
