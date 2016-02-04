801c64a8103d8d467e3c2572fba813ff3bbd5d2d
John Lim <jolim24601@gmail.com>
2016-02-04 03:14:22 -0500

    Configure env vars..

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
     secure_image_url: true
 
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
