bea0b48e2f124cd72fb466bd2a7bd4152c9f9f13
John Lim <jolim24601@gmail.com>
2016-02-25 14:53:49 -0500

    Simply issue a redirect back to auth form if there is a conflict in oauth between Twitter and FB.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,11 @@
 class SessionsController < ApplicationController
   def create
-    @author = Author.from_omniauth(request.env['omniauth.auth'])
-    if @author.nil?
-      render json: { errors: ["Something went wrong."] }, status: 401
+    begin
+      @author = Author.from_omniauth(request.env['omniauth.auth'])
+    rescue => e
+      # render json: { errors: e.message }, status: 401
+      # redirect back to page for now
+      redirect_to root_url + '#/auth'
     else
       login_author(@author)
       redirect_to root_url + '#/'
