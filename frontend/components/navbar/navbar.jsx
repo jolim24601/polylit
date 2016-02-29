c0b654fe3ac7f4ab1300a8660a743fe65e58f886
John Lim <jolim24601@gmail.com>
2016-02-29 17:07:37 -0500

    Remove hide from sidebar after side-right is added. It then becomes unnecessary.

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -52,12 +52,11 @@ var Navbar = React.createClass({
     var promo =  document.querySelector('.promotron');
     var sb =  document.querySelector('.sidebar');
 
-    // remove initialized display
-    sb.classList.remove('hide');
-
     // check if they scrolled past the promo or promo is there but not loaded (i.e. 0 height)
     if (promo && (st < promo.offsetHeight || promo.offsetHeight === 0)) {
       sb.classList.add('side-right');
+      // remove initialized display
+      sb.classList.remove('hide');
     } else {
       sb.classList.remove('side-right');
     }
